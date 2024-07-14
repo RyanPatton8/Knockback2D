5a560c072859b3ded23df4c9f95b8b84d70779a1
Ryan Patton <ryanpatton173@gmail.com>
2024-07-14 16:24:05 -0400

    Basic Attack Registering

--- Scripts/PlayerScripts/player.cs
+++ Scripts/PlayerScripts/player.cs
@@ -6,21 +6,36 @@ public partial class Player : RigidBody2D
     [Export] public int playerIndex {get; set;}
 	[Export] public Area2D GroundCheck { get; private set; }
 	[Export] public float maxMoveSpeed { get; private set; }
-	//[Export] public Sprite2D HitSprite { get; private set; }
+    [Export] public Area2D HitBox { get; private set; }
+	[Export] public Area2D HurtBox { get; private set; }
 
+    [Export] public Timer AttackDuration {get; private set;}
+    [Export] public Timer AttackCoolDown {get; private set;}
+
+    private float offsetAmount = 21;
 	private bool isGrounded = false;
+    private bool canAttack = true;
+
+    
 
 	public override void _Ready()
 	{
 		GroundCheck.BodyEntered += Grounded;
 		GroundCheck.BodyExited += UnGrounded;
+        HurtBox.AreaEntered += RecieveHit;
+        AttackDuration.Timeout += StopAttacking;
+        AttackCoolDown.Timeout += ResetAttack;
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		//Aim();
+        Move();
+		Aim();
+	}
 
-		if (!Input.IsJoyButtonPressed(playerIndex, JoyButton.A) && isGrounded)
+    private void Move()
+    {
+        if (!Input.IsJoyButtonPressed(playerIndex, JoyButton.A) && isGrounded)
 		{
 			PhysicsMaterialOverride.Friction = 0.6f;
 		}
@@ -39,7 +54,7 @@ public partial class Player : RigidBody2D
 		{
 			ApplyForce(new Vector2(15000 * direction, 0));
 		}
-	}
+    }
 
 	private void Grounded(Node body)
 	{
@@ -56,17 +71,58 @@ public partial class Player : RigidBody2D
 		}
 	}
 
-	// private void Aim()
-	// {
-	// 	float aimX = Input.GetActionStrength("aimRight") - Input.GetActionStrength("aimLeft");
-	// 	float aimY = Input.GetActionStrength("aimDown") - Input.GetActionStrength("aimUp");
+	private void Aim()
+	{
+		float aimX = Input.GetJoyAxis(playerIndex, JoyAxis.RightX);
+		float aimY = Input.GetJoyAxis(playerIndex, JoyAxis.RightY);
 
-	// 	Vector2 aimDirection = new Vector2(aimX, aimY);
+		Vector2 aimDirection = new Vector2(aimX, aimY);
 
-	// 	 if (aimDirection.Length() > 0.3)
-    // 	{
-	// 		aimDirection = aimDirection.Normalized();
-	// 		HitSprite.RotationDegrees = Mathf.RadToDeg(GlobalPosition.AngleToPoint(GlobalPosition + aimDirection));
-    // 	}
-	// }
+		 if (aimDirection.Length() > 0.3)
+        {
+            aimDirection = aimDirection.Normalized();
+
+            // Apply the offset to the aimDirection
+            Vector2 newPosition = GlobalPosition + aimDirection * offsetAmount;
+
+            // Rotate the HurtBox around the player
+            HitBox.GlobalPosition = newPosition;
+            HitBox.RotationDegrees = Mathf.RadToDeg(GlobalPosition.AngleToPoint(newPosition));
+        }
+
+        if(Input.IsJoyButtonPressed(playerIndex, JoyButton.RightShoulder))
+        {
+            Attack();
+        }
+	}
+
+    private void Attack()
+    {
+        if(canAttack){
+            GD.Print("Attacked");
+            canAttack = false;
+            HitBox.Monitoring = true;
+            HitBox.Monitorable = true;
+            AttackDuration.Start();
+        }
+    }
+
+    private void RecieveHit(Area2D area)
+    {
+        ApplyImpulse(new Vector2(0,1) * 10);
+        ApplyImpulse((GlobalPosition - area.GlobalPosition) * 500);
+        GD.Print("Ouch");
+    }
+
+    private void StopAttacking()
+    {
+        HitBox.Monitoring = false;
+        HitBox.Monitorable = false;
+        AttackCoolDown.Start();
+    }
+
+    private void ResetAttack()
+    {
+        canAttack = true;
+    }
 }
