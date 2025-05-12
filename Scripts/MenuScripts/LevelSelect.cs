330feff18f14bcfd8b57d085569a2f4dd2ec261f
Ryan Patton <ryanpatton173@gmail.com>
2025-05-12 14:13:57 -0400

    added ability to return to readyup from levelselect

---
+++ Scripts/MenuScripts/LevelSelect.cs
@@ -0,0 +1,22 @@
+using Godot;
+using System;
+
+public partial class LevelSelect : Control
+{
+	// Called when the node enters the scene tree for the first time.
+	PlayerManager playerManager;
+	GameManager gameManager;
+	public override void _Ready()
+	{
+		playerManager = PlayerManager.Instance;
+		gameManager = GameManager.Instance;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if(Input.IsJoyButtonPressed(0, JoyButton.B) && playerManager.playerList.Count > 0){
+			gameManager.ReadyUp();
+		}
+	}
+}
